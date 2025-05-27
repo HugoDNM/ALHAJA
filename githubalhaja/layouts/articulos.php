7b343f2e68fbd26169d01ea6b3eea7267c556e63
HugoDNM <hn11798@gmail.com>
2025-05-27 15:41:02 +0200

    Delete githubalhaja directory

--- githubalhaja/layouts/articulos.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="../css/articulo1.css" />
-
-
-<div class="contenedor-carrusel">
-    <div class="flecha-izquierda" onclick="cambiarImagen(-1)">&#9665;</div>
-    <div class="flecha-derecha" onclick="cambiarImagen(1)">&#9655;</div>
